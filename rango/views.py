48113f62e1e49e267a59f8bddfb23d03a6df288c
Ubuntu <ubuntu@ip-172-31-38-202.us-west-2.compute.internal>
2015-03-31 04:31:52 +0000

    Done some listing

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     context_dict = {'boldmessage': "I am bold font from the context"}
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict['categories'] = category_list
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
